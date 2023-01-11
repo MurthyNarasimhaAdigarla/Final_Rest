3c3bc32a7876a878a0c96fddc6e7c37e81581f87
MurthyNarasimhaAdigarla <murthy.adigarlaa@gmaail.com>
2023-01-11 12:16:17 +0530

    first commit

---
+++ src/test/java/practise/com/PractiseTest.java
@@ -0,0 +1,39 @@
+package practise.com;
+
+import org.json.simple.JSONObject;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.baseURI;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PractiseTest {
+
+
+    // @Test
+    public void testt() {
+
+        baseURI = "https://reqres.in/api";
+
+        given().get("users?page=2").then().statusCode(200).body("data[4].first_name", equalTo("George"));
+
+        System.out.println(given().get("users?page=2").then().statusCode(200).body("data[4].first_name", equalTo("George")));
+
+    }
+
+    @Test
+    public void posttt() {
+
+        ////        Map<String, Object> map = new HashMap<>();
+        ////        map.put("name", "morpheus");
+        ////        map.put("job", "leader");
+        //        System.out.println(map);
+        JSONObject request = new JSONObject();
+        request.put("name", "morpheus");
+        request.put("job", "leader");
+        baseURI = "https://reqres.in/api";
+        given().body(request.toJSONString()).when().post("/users?page=2").then().statusCode(201).log().all();
+    }
+
+}
+
